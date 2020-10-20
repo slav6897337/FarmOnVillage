101d435a0dd35aad811df41b2261b40f88238188
slav6897337 <slava6897337@gmail.com>
2020-10-20 21:12:19 +0300

    1

--- FarmOnVillage/Farm.cs
+++ FarmOnVillage/Farm.cs
@@ -40,6 +40,56 @@ namespace FarmOnVillage
             Console.WriteLine($"Thesis farm {NameFarm}, area {Area}, {BildingFarm.Count} building and {GardenBedFarm.Count} Garden Beds");
         }
 
+        /// <summary>
+        /// This Method calculate used area on farm.
+        /// </summary>
+        /// <returns>usedAreaOnFarm.</returns>
+        private int UsedAreaOnFarm()
+        {
+            int usedAreaOnFarm = 0;
+            foreach (var item in GardenBedFarm)
+            {
+                usedAreaOnFarm += item.Square;
+            }
 
+            foreach (var item in BildingFarm)
+            {
+                usedAreaOnFarm += item.AriaOfBilding;
+            }
+
+            return usedAreaOnFarm;
+        }
+
+        /// <summary>
+        /// This Method add bild on farm.
+        /// </summary>
+        /// <param name="val"></param>
+        public void AddBildOnFarm(Bilding val)
+        {
+            if (UsedAreaOnFarm() + val.AriaOfBilding < Area)
+            {
+                BildingFarm.Add(val);
+            }
+            else
+            {
+                Console.WriteLine("Sorry Area busy.");
+            }
+        }
+
+        /// <summary>
+        /// This Method add bad on farm.
+        /// </summary>
+        /// <param name="val"></param>
+        public void AddBadOnFarm(GardenBed val)
+        {
+            if (UsedAreaOnFarm() + val.Square < Area)
+            {
+                GardenBedFarm.Add(val);
+            }
+            else
+            {
+                Console.WriteLine("Sorry Area busy.");
+            }
+        }
     }
 }
